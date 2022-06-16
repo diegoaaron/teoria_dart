89776c490cc67e0ffda2204687b4323fe36b322f
Diego Aarón <diegoaarondv@gmail.com>
2022-06-16 15:03:31 -0500

    update trabajo 1.1 tipos integrados

--- bin/teoria_dart.dart
+++ bin/teoria_dart.dart
@@ -2,4 +2,9 @@ import 'package:teoria_dart/teoria_dart.dart' as teoria_dart;
 
 void main(List<String> arguments) {
   print('Hello world: ${teoria_dart.calculate()}!');
+
+  var x = 1;
+  var hex = 0xDEADBEEF;
+
+  print(hex);
 }
